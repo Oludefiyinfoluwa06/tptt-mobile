f3bbc10a8da22c726d47d2f2f26f2ab2c848d836
Olude Fiyinfoluwa <oludefiyinfoluwa06@gmail.com>
2026-09-09 13:35:46 +0100

    Update env.ts

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,1 +1,2 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:5000/api';
+// export const API_URL = process.env.EXPO_PUBLIC_API_URL ?? 'http://192.168.0.3:3000/api';
+export const API_URL = 'http://192.168.0.3:3000/api';
