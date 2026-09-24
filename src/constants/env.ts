03d5d1aeac71d2b6bcd4b5b688510bf2cfb5646f
Olude Fiyinfoluwa <oludefiyinfoluwa06@gmail.com>
2026-09-24 17:37:42 +0100

    Update API URL

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,2 +1,2 @@
-// export const API_URL = process.env.EXPO_PUBLIC_API_URL ?? 'http://192.168.0.3:3000/api';
-export const API_URL = 'http://192.168.0.3:5000/api';
+// export const API_URL = process.env.EXPO_PUBLIC_API_URL ?? 'https://tptt-api.vercel.app/api';
+export const API_URL = 'https://tptt-api.vercel.app/api';
